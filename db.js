1f4ec6722b3374fd227bfdc270d3e968bb469377
CeutaRossmary <77645395+CeutaRossmary@users.noreply.github.com>
2022-03-20 00:27:26 -0300

    pg - node

---
+++ db.js
@@ -0,0 +1,99 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    user: "postgres",
+    host: "localhost",
+    database: "gym",
+    password: "1234",
+    max: 12,
+    min: 2,
+    idleTimeoutMillis: 3000,
+    connectionTimeoutMillis: 2000,
+});
+
+async function get_now() {
+    // primero solicitamos un cliente
+    const client = await pool.connect();
+
+    // después realizamos la consulta
+    const { rows } = await client.query("select now()");
+
+    // más adelante liberamos el cliente
+    client.release();
+
+    return rows[0];
+}
+
+async function insertar(nombre, series, repeticiones, descanso) {
+    // primero solicitamos un cliente
+    const client = await pool.connect();
+
+    // después realizamos la consulta
+    const { rows } = await client.query({
+        text: `insert into ejercicios (nombre, series, repeticiones, descanso) values ($1, $2, $3, $4)
+          returning *`,
+        values: [
+            nombre,
+            parseInt(series),
+            parseInt(repeticiones),
+            parseInt(descanso),
+        ],
+    });
+
+    // más adelante liberamos el cliente
+    client.release();
+
+    return rows[0];
+}
+
+async function actualizar(nombre, series, repeticiones, descanso) {
+    // primero solicitamos un cliente
+    const client = await pool.connect();
+
+    // después realizamos la consulta
+    const { rows } = await client.query({
+        text: `update ejercicios set series=$2, repeticiones=$3, descanso=$4 where nombre=$1
+           returning *`,
+        values: [
+            nombre,
+            parseInt(series),
+            parseInt(repeticiones),
+            parseInt(descanso),
+        ],
+    });
+
+    // más adelante liberamos el cliente
+    client.release();
+
+    return rows[0];
+}
+
+async function eliminar(nombre) {
+    // primero solicitamos un cliente
+    const client = await pool.connect();
+
+    // después realizamos la consulta
+    await client.query({
+        text: `delete from ejercicios where nombre=$1`,
+        values: [nombre],
+    });
+
+    // más adelante liberamos el cliente
+    client.release();
+    return;
+}
+
+async function consultar() {
+    // primero solicitamos un cliente
+    const client = await pool.connect();
+
+    // después realizamos la consulta
+    const { rows } = await client.query("select * from ejercicios");
+
+    // más adelante liberamos el cliente
+    client.release();
+
+    return rows;
+}
+
+module.exports = { get_now, insertar, consultar, actualizar, eliminar };
---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require('express')
+const { get_now, insertar, consultar, actualizar, eliminar } =
+require('./db.js')
+
+const app = express()
+app.use(express.static('static'))
+app.use(express.static("node_modules/bootstrap/dist"));
+
+app.get('/now', async(req, res) => {
+    // primero realizo la llamada a la función
+    const ahora = await get_now()
+        // después retorno
+    res.json(ahora)
+})
+
+app.get('/ejercicios', async(req, res) => {
+    const ejercicios = await consultar()
+    res.json({ rows: ejercicios })
+})
+
+app.post('/ejercicios', async(req, res) => {
+    let body = ''
+
+    // Las funciones 'on' sirven para desempaquetar llamas AJAX cuyos valores se pasan como un String.
+    // esta estructura siempre es igual en los PUT y POST
+    req.on('data', (data) => body += data)
+
+    req.on('end', async() => {
+        console.log(body);
+        // primero parseamos los datos que llegan
+        body = JSON.parse(body)
+
+        // ahora llamamos a la función de base de datos
+        await insertar(body.nombre, body.series, body.repeticiones, body.descanso)
+
+        // TODAS las rutas deben responder
+        res.status(201).json({ todo: 'ok' })
+    })
+
+})
+
+app.put('/ejercicios', async(req, res) => {
+    let body = ''
+
+    // Las funciones 'on' sirven para desempaquetar llamas AJAX cuyos valores se pasan como un String.
+    // esta estructura siempre es igual en los PUT y POST
+    req.on('data', (data) => body += data)
+
+    req.on('end', async() => {
+        // primero parseamos los datos que llegan
+        body = JSON.parse(body)
+        console.log(body);
+
+        // ahora llamamos a la función de base de datos
+        await actualizar(body.nombre, body.series, body.repeticiones, body.descanso)
+
+        // TODAS las rutas deben responder
+        res.status(201).json({ todo: 'ok' })
+    })
+})
+
+app.delete('/ejercicios', async(req, res) => {
+    console.log(req.query);
+    // 1. Obtenemos el nombre del ejercicio a eliminar desde el QueryString
+    const nombre = req.query.nombre
+
+    // 2. Llamamos a la función eliminar
+    await eliminar(nombre)
+
+    res.send({ todo: 'ok' })
+})
+
+app.listen(3000, () => console.log('servidor ejecutando en puerto 3000'))
